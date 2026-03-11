f63603c4656f1b04de3c62ce2c2d6eacebfa14b4
Compu-IPM <alumno27.lopez.sebastian@ipm.edu.ar>
2026-03-11 08:18:20 -0300

    correccion del ejercicio 2

--- ej2 6_03.cpp
+++ ej2 6_03.cpp
@@ -9,6 +9,19 @@ struct producto_t{
     double precio;
     bool fecha_de_vencimiento;
 };
+float promedio(int & cantidad,float total){
+    float promedio = 0;
+    promedio = total/cantidad;
+    cantidad = 0;
+    return promedio;
+}
+void mayorcito(producto_t producto,producto_t & mayor_producto,int & mayor_columna,int columna){
+    if(producto.precio > mayor_producto.precio) {
+        mayor_producto = producto;
+        mayor_columna = columna+1;
+    }
+}
+
 
 int main() {
     producto_t mayor_producto;
@@ -20,23 +33,15 @@ int main() {
         {{"Queso", 345678, "Marca C", 2.5, false},{"Yogur", 456789, "Marca D", 1.0, false},{"Mantequilla", 567890, "Marca E", 3.0, false},{"Huevos", 678901, "Marca F", 2.5, true}},
         {{"Carne", 567890, "Marca E", 5.0, false},{"Pescado", 678901, "Marca F", 4.0, false},{"Pollo", 789012, "Marca G", 3.5, true},{"Cerdo", 890123, "Marca H", 4.5, false}}
     }));
-    for(int pasillo = 0; pasillo < supermercado.size(); pasillo++) {
-        for(int filas = 0; filas < supermercado[pasillo].size(); filas++) {
-            for(int columna = 0; columna < supermercado[pasillo][filas].size(); columna++) {
+        for(int filas = 0; filas < supermercado[0].size(); filas++) {
+            for(int columna = 0; columna < supermercado[0][filas].size(); columna++) {
                 contador_productos++;
-                if(supermercado[pasillo][filas][columna].precio > mayor_producto.precio) {
-                    mayor_producto = supermercado[pasillo][filas][columna];
-                    columna_mayor_precio = columna+1;
-                }
-                if(supermercado[pasillo][filas][columna].fecha_de_vencimiento) {
-                    contador_productos_fecha_vencimiento++;
-                }
-                promedio_precio += supermercado[pasillo][filas][columna].precio;
+                mayorcito(supermercado[0][filas][columna],mayor_producto,columna_mayor_precio,columna);
+                if(supermercado[0][filas][columna].fecha_de_vencimiento) contador_productos_fecha_vencimiento++;
+                promedio_precio += supermercado[0][filas][columna].precio;
             }
         }
-        promedio_precio /= contador_productos;
-        contador_productos = 0;
-    }
+        promedio(contador_productos,promedio_precio);
     cout<<"Columna con el producto de mayor precio: "<<columna_mayor_precio<<endl;
     cout<<"Nombre del producto de la fila 2 columna 3: "<<supermercado[0][1][2].nombre<<endl;
     cout<<"Promedio de precios: "<<promedio_precio<<endl;
