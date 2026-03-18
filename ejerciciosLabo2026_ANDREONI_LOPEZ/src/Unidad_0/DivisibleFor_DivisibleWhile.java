a3da7e769a87b4a783c42403fd36efbb33d86085
Compu-IPM <alumno27.LOPEZ.Sebastian@ipm.edu.ar>
2026-03-18 09:33:58 -0300

    corregidos los ejercicios del 18-03

--- ejerciciosLabo2026_ANDREONI_LOPEZ/src/Unidad_0/DivisibleFor_DivisibleWhile.java
+++ ejerciciosLabo2026_ANDREONI_LOPEZ/src/Unidad_0/DivisibleFor_DivisibleWhile.java
@@ -3,10 +3,18 @@ package Unidad_0;
 public class DivisibleFor_DivisibleWhile {
     public static void main(String[] args) {
         for(int i=1; i<101;i++){
-            if(i % 3 == 0 || i % 2 ==0){
+            if(i % 3 == 0 && i % 2 ==0){
                 System.out.println(i);
 
             }
+        }
+        int i =1;
+        while(i < 101){
+            if(i % 3 == 0 && i % 2 ==0){
+                System.out.println(i);
+
+            }
+            i++;
         }
     }
 }
