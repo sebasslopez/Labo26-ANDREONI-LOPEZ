badba36527d0799f1643d592428296f900f99974
Compu-IPM <alumno27.LOPEZ.Sebastian@ipm.edu.ar>
2026-03-18 09:56:53 -0300

    corregidos los ejercicios del 18-03

--- ejerciciosLabo2026_ANDREONI_LOPEZ/src/Unidad_0/IntroducirNumeros.java
+++ ejerciciosLabo2026_ANDREONI_LOPEZ/src/Unidad_0/IntroducirNumeros.java
@@ -1,7 +1,16 @@
 package Unidad_0;
 
+import java.util.Scanner;
+
 public class IntroducirNumeros {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        int numero = s.nextInt();
+        int contador = 0;
+        while ( numero != -1){
+            contador++;
+            numero = s.nextInt();
+        }
+        System.out.println(contador);
     }
 }
