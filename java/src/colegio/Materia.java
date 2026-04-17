3b3de23fe6674f3cefaa49af4312da55bc4c0c28
Compu-IPM <alumno27.LOPEZ.Sebastian@ipm.edu.ar>
2026-04-17 13:32:13 -0300

    practica de arrays, hechos hasta el 4

--- java/src/colegio/Materia.java
+++ java/src/colegio/Materia.java
@@ -2,6 +2,8 @@ package colegio;
 
 import gente.Alumno;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 
 public class Materia {
@@ -14,10 +16,47 @@ public class Materia {
         this.alumnos=new ArrayList<>();
         this.contenidos=new ArrayList<>();
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<String> getContenidos() {
+        return contenidos;
+    }
+
+    public void setContenidos(ArrayList<String> contenidos) {
+        this.contenidos = contenidos;
+    }
+
+    public ArrayList<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(ArrayList<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+
     public int promedioEdad(){
         int total = 0;
-        for(Alumno a : alumnos){
-
+        for(int i=0; i<alumnos.size();i++){
+            total+=(Period.between(LocalDate.of(alumnos.get(i).getFechan().getYear(),alumnos.get(i).getFechan().getMes(),alumnos.get(i).getFechan().getDia()), LocalDate.now()).getYears());
         }
+        return total/alumnos.size();
+    }
+    public int promedioNotas(){
+        int total = 0;
+        int contador = 0;
+        for (int j = 0; j<alumnos.size();j++){
+            for(int i =0;i<alumnos.get(j).getNotas().size();i++){
+                total += alumnos.get(j).getNotas().get(i);
+                contador++;
+            }
+        }
+        return total/contador;
     }
 }
