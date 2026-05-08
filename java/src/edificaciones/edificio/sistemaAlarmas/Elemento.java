c146e759b44db977a656efab4610e2964a1a6036
Compu-IPM <alumno27.LOPEZ.Sebastian@ipm.edu.ar>
2026-05-08 17:09:02 -0300

    Cambiado algo del ejercicio 2 y movido el paquete Museo a Edificaciones

--- java/src/edificaciones/edificio/sistemaAlarmas/Elemento.java
+++ java/src/edificaciones/edificio/sistemaAlarmas/Elemento.java
@@ -6,4 +6,42 @@ public class Elemento {
     private float umbral;
     private int anio;
 
+    public Elemento(boolean conectado, float umbral, int anio) {
+        this.conectado = conectado;
+        this.medida = medida;
+        this.umbral = umbral;
+        this.anio = anio;
+    }
+
+    public boolean isConectado() {
+        return conectado;
+    }
+
+    public void setConectado(boolean conectado) {
+        this.conectado = conectado;
+    }
+
+    public float getMedida() {
+        return medida;
+    }
+
+    public void setMedida(float medida) {
+        this.medida = medida;
+    }
+
+    public float getUmbral() {
+        return umbral;
+    }
+
+    public void setUmbral(float umbral) {
+        this.umbral = umbral;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
 }
